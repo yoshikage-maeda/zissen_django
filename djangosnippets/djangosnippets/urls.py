db78a24692f4692b4329e01146357971d5522df3
jamuojisan <kage0427@gmail.com>
2023-10-23 23:34:12 +0900

    [add] 1.3節まで読了

--- djangosnippets/djangosnippets/urls.py
+++ djangosnippets/djangosnippets/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from snippets.views import top
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', top, name='top'),
+    path('snippets/', include('snippets.urls')), # snippets/urls.py の読み込み
+    path('admin/', admin.site.urls)
 ]
